37d02ad59e3db63ba4d66b0ea76dbb274432c7f8
J4nush <michal.ziolek@pollub.edu.pl>
2023-10-03 17:02:39 +0200

    Initial Commit

---
+++ SWIFTCalculator/SWIFTCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTCalculatorApp.swift
+//  SWIFTCalculator
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
